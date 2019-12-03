aea415b894504a399af3c8d00dee1d933c2d5bc6
SeptemberHX <september_hx@outlook.com>
2019-12-03 16:54:48 +0800

    f

--- src/main/java/com/sampleservice/ofoservice/controller/MainController.java
+++ src/main/java/com/sampleservice/ofoservice/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.sampleservice.ofoservice.controller;
 
+import com.sampleservice.ofoservice.utils.MBaseUtils;
 import com.septemberhx.common.base.MResponse;
 import com.septemberhx.mclient.annotation.MApiFunction;
 import com.septemberhx.mclient.annotation.MRestApiType;
@@ -20,6 +21,10 @@ public class MainController extends MObject {
     public MResponse shareBikeFunction(@RequestBody MResponse requestData) {
         MResponse result = new MResponse();
         result.set("msg", "/shareBike");
+
+        if (requestData.get("interval") != null) {
+            MBaseUtils.generateStringInKBSize(1500, result, (int) requestData.get("interval"));
+        }
         return result;
     }
 }
